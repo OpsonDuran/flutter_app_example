85dc399fe8fbe15b7c65bbb998bcdfde8a27940d
armando.duran2 <armando.duran2@bbva.com>
2019-04-26 23:49:44 -0500

    add MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Directionality(
-  textDirection: TextDirection.ltr,
-  child: Center(
-      child:Text("Hello World"),
-  ),
-  )
-);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Add Material Dart with theme dark
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Container(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+
+}
 
